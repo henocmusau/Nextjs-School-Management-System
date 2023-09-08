7775ad7f9265746bc61a59e3d3106186f178ba52
Henoc Musau <henocmusau@gmail.com>
2023-09-08 22:08:30 +0100

    Ajout de la section filtre

---
+++ components/ListBox.jsx
@@ -0,0 +1,79 @@
+'use client'
+import { Fragment, useEffect, useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+// import { CheckIcon, } from '@heroicons/react/20/solid'
+import { FiChevronDown as ChevronUpDownIcon } from 'react-icons/fi'
+import { AiOutlineCheck as CheckIcon } from 'react-icons/ai'
+
+const people = [
+    { id: 0, name: '1ère Primaire' },
+    { id: 1, name: '2eme Primaire' },
+    { id: 2, name: '3ème Primaire' },
+    { id: 3, name: '4ème Primaire' },
+    { id: 4, name: '5ème Primaire' },
+    { id: 5, name: '6ème Primaire' },
+]
+
+export default function ListBox() {
+    const [selected, setSelected] = useState(people[0])
+
+    useEffect(() => {
+        console.log(selected.name)
+
+
+    }, [selected])
+
+
+    return (
+        <div className="">
+            <Listbox value={selected} onChange={setSelected}>
+                <div className="relative mt-1">
+                    <Listbox.Button className="h-full my-auto shadow-md relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left border border-gray-300 focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                        <span className="block truncate">{selected.name}</span>
+                        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                            <ChevronUpDownIcon
+                                className="h-5 w-5 text-gray-400"
+                                aria-hidden="true"
+                            />
+                        </span>
+                    </Listbox.Button>
+                    <Transition
+                        as={Fragment}
+                        leave="transition ease-in duration-100"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                            {people.map((person) => (
+                                <Listbox.Option
+                                    key={person.id}
+                                    className={({ active }) =>
+                                        `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                                        }`
+                                    }
+                                    value={person.name}
+                                >
+                                    {({ selected }) => (
+                                        <>
+                                            <span
+                                                className={`block truncate ${selected ? 'font-medium' : 'font-normal'
+                                                    }`}
+                                            >
+                                                {person.name}
+                                            </span>
+                                            {selected ? (
+                                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                </span>
+                                            ) : null}
+                                        </>
+                                    )}
+                                </Listbox.Option>
+                            ))}
+                        </Listbox.Options>
+                    </Transition>
+                </div>
+            </Listbox>
+        </div>
+    )
+}
--- components/StudentsFilters.jsx
+++ components/StudentsFilters.jsx
@@ -1,9 +1,35 @@
+'use client'
+
 import React from 'react'
+import { AiOutlineSearch } from 'react-icons/ai'
+import Listbox from './ListBox'
+
+
 
 export default function StudentsFilters() {
+    function handleChange() {
+
+    }
     return (
-        <section className='bg-white text-slate-900 rounded-t-xl pt-8 pl-12 mt-6'>
-            filtres
+        <section className='bg-white text-slate-900 relative rounded-t-xl pt-8 px-12 mt-6 flex items-center'>
+            <div className='relative basis-5/12 mr-6'>
+                <input
+                    type='text'
+                    className="border-2 w-full border-gray-300 bg-white h-10 px-8 rounded-lg text-sm focus:outline-none"
+                    name='filterText'
+                    value={''}
+                    placeholder="Entrez le nom de l'élève que vous recherchez ici..."
+                    onChange={handleChange}
+                    autoComplete='off'
+                />
+                <button
+                    type="button"
+                    className="absolute left-0 top-0 my-0 h-full ml-2 text-slate-400"
+                >
+                    <AiOutlineSearch className='text-2xl' />
+                </button>
+            </div>
+            <Listbox />
         </section>
     )
 }
