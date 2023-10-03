847c2d5d287a321664ae9fb6a2698de167e4bddc
Henoc Musau <henocmusau@gmail.com>
2023-10-03 02:52:10 +0100

    modification des noms des variables d'environnement

--- models/sequelizeDB.js
+++ models/sequelizeDB.js
@@ -2,11 +2,11 @@ import { Sequelize } from "sequelize";
 import * as pg from 'pg'
 
 const schoolXDB = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
+    process.env.POSTGRES_DATABASE,
+    process.env.POSTGRES_USER,
+    process.env.POSTGRES_PASSWORD,
     {
-        host: process.env.DB_HOST,
+        host: process.env.POSTGRES_HOST,
         port: process.env.DB_PORT,
         dialect: 'postgres',
         dialectModule: pg
