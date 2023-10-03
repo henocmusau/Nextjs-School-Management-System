212ba3b5d52ecc7b213cd2ffbaa6e0fe198f64b9
Henoc Musau <henocmusau@gmail.com>
2023-10-03 01:59:37 +0100

    Ajout de la page student/[id], correction de quelques défauts de design

--- components/StudentRow.jsx
+++ components/StudentRow.jsx
@@ -1,15 +1,28 @@
 import Link from "next/link"
 import TableRow from "./TableRow"
+import DropdownMenu from "./DropdownMenu"
 
 export default function StudentRow({ student, isClass }) {
     return (
         <TableRow>
-            <td className='py-2 pl-4 uppercase'>{`${student?.lastName} ${student?.middleName} ${student?.firstName}`}</td>
+            <td
+                className='py-2 pl-4 uppercase hover:text-primary dark:hover:text-primaryDark'
+            >
+                <Link href={`/students/${student?.id}`}>
+                    {student?.fullName}
+                </Link>
+            </td>
             {!isClass ? <td>
-                <Link href={`/classes/${student.class.id}`} className="cursor-pointer h-full w-full hover:text-secondary duration-200">
+                <Link
+                    href={`/classes/${student.class.id}`}
+                    className="cursor-pointer h-full w-full hover:text-secondary duration-200"
+                >
                     {student.class.label}
                 </Link>
             </td> : null}
+            <td className="items-start">
+                <DropdownMenu />
+            </td>
         </TableRow>
     )
 }
--- components/TableRow.jsx
+++ components/TableRow.jsx
@@ -1,6 +1,6 @@
 export default function TableRow({ children }) {
     return (
-        <tr className='even:bg-white odd:transparent dark:even:bg-secondaryDark/60 dark:odd:bg-transparent'>
+        <tr className='even:bg-white relative odd:transparent dark:even:bg-secondaryDark/60 dark:odd:bg-transparent'>
             {children}
         </tr>
     )
